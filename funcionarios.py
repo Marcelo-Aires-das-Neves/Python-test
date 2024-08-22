c2b9c59289e25c2874a6e587f3eb19ae4ad11807
marcelo aires das neves <mneves230@gmail.com>
2024-08-22 18:11:41 -0300

    tesss

--- funcionarios.py
+++ funcionarios.py
@@ -4,8 +4,10 @@ while True:
     nome = input('Nome: ')
     if nome != 'fim':
         counter += 1
-        users[counter] = nome
+        users[counter] = [nome, int(input('Idade: '))]
     else:
         break
+for v in users.values():
+    print(v[0], v[1])    
 
 print(users)
