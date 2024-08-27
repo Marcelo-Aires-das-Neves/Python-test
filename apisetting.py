273cc756e32a0d581762126838e7ae5e299b91e1
marcelo aires das neves <mneves230@gmail.com>
2024-08-27 08:51:21 -0300

    testapi in python

---
+++ apisetting.py
@@ -0,0 +1,19 @@
+from requests  import Session
+import json
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+response = session.get(url, params=parameters)
+data = json.loads(response.text)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+s://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+response = session.get(url, params=parameters)
+data = json.loads(response.text)
