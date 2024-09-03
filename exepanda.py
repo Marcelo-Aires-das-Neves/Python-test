74c4c7cd1a2b565e5b11800f1deecae5d1fe147d
marcelo aires das neves <mneves230@gmail.com>
2024-09-03 19:00:41 -0300

    Exemplo Panda testing

--- exepanda.py
+++ exepanda.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 dados = pd.read_excel('C:/Users/USER/Documents/dditatech/arquivo1.xlsx')
 a = dados.head(6)
-print(a)
+b = dados.columns.tolist()     # mostra as colunas 
+print(b)
