ae914b414019c68bf42998083781dcc0a8b4108f
marcelo aires das neves <mneves230@gmail.com>
2024-09-06 18:56:26 -0300

    test13

--- execlassific.py
+++ execlassific.py
@@ -3,9 +3,14 @@ import pandas as pd
 df = pd.read_csv('C:/Users/USER/Downloads/archive/athlete_events.csv')
 a = df.head(6)
 
-total = len(df)
-m = (len(df['Sex'].loc[df['Sex'] == 'M'])) / total
-f =(len(df['Sex'].loc[df['Sex'] == 'F'])) / total
+#total = len(df)
+m = df['Sex'].value_counts()[0] / len(df)
+f = df['Sex'].value_counts()[1] / len(df)
+#print(total)
+
+
+#m = (len(df['Sex'].loc[df['Sex'] == 'M'])) / total
+#f =(len(df['Sex'].loc[df['Sex'] == 'F'])) / total
 #in this part we calculate the percentage of men and women in the dataset, using the :.2f to round the percentage to 2 decimal places
 print(f'The percentage of men is {m * 100:.2f}%')
 print(f'The percentage of women is {f * 100:.2f}%')
