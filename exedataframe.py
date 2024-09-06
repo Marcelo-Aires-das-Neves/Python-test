0287c20476f7c1c32fee739d1a8d6e9e63ea58ae
marcelo aires das neves <mneves230@gmail.com>
2024-09-06 14:20:10 -0300

    Test5

--- exedataframe.py
+++ exedataframe.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-alunos = {'Nome': ['Ricardo', 'Pedro', 'Roberto', 'Carlos'],
-          'Nota': [7.5, 9, 6.5, 10],
-          'Aprovado': [True, False, True, True]}
+alunos = {'Nome': ['Ricardo', 'Pedro', 'Roberto', 'Carlos', 'Ricardo'],
+          'Nota': [7.5, 9, 6.5, 10,8],
+          'Aprovado': [True, False, True, True, True]}
 
 df = pd.DataFrame(alunos, columns=['Nome', 'Nota', 'Aprovado'])
 print(df)
