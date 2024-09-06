0c9aba51aa582e4eab0cacce2d8eb018e19373fc
marcelo aires das neves <mneves230@gmail.com>
2024-09-06 17:44:35 -0300

    test12

--- execlassific.py
+++ execlassific.py
@@ -6,9 +6,14 @@ a = df.head(6)
 total = len(df)
 m = (len(df['Sex'].loc[df['Sex'] == 'M'])) / total
 f =(len(df['Sex'].loc[df['Sex'] == 'F'])) / total
-
+#in this part we calculate the percentage of men and women in the dataset, using the :.2f to round the percentage to 2 decimal places
 print(f'The percentage of men is {m * 100:.2f}%')
 print(f'The percentage of women is {f * 100:.2f}%')
 
+"""
+In this part define the round function to round the percentage to 2 decimal places
+print(f'The percentage of men is {round(m * 100)}%')
+print(f'The percentage of women is {round(f * 100)}%')
+"""
 
 
