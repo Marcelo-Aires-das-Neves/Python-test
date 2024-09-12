1046dca32048f2c8ededf752469a15497f383574
marcelo aires das neves <mneves230@gmail.com>
2024-09-12 20:43:33 -0300

    test

--- test_flask.py
+++ test_flask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world() -> str:
  
-    return 'Hello, Marcelo Aires!'
+    return 'Hello, Marcelo Neves!'
 
 if __name__ == "__main__":
     app.run(debug=True)
