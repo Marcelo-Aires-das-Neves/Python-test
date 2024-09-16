864861beaf28f365fc4e3140c58cca660eecd6ea
marcelo aires das neves <mneves230@gmail.com>
2024-09-16 20:09:20 -0300

    test

--- exe_class.py
+++ exe_class.py
@@ -1,15 +1,16 @@
 class Cachorro:
     def __init__(self, nome: str, raca: str, cor: str):
-        
+        #atributos
         self.nome = nome
         self.raca = raca
         self.cor = cor
+    #metodos
     def latir(self):
         print('au au')
-    
+    #metodos
     def correr(self):
         print('correndo')
-                
+    #metodos            
     #criando objetos , instancias da classe
 cachorro1 = Cachorro('rex', 'labrador', 'branco')
 cachorro2 = Cachorro('scooby', 'pitbull', 'preto')
