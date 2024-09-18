ac0b2f8ae6e894c106ac802c2f084ff4f2ddf74d
marcelo aires das neves <mneves230@gmail.com>
2024-09-17 21:35:59 -0300

    test

--- exepanda.py
+++ exepanda.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 dados = pd.read_csv('C:/Users/USER/Downloads/archive/athlete_events.csv')
+b = dados.groupby('Team').agg({'Name': 'nunique', 
+                           'Medal': 'count',
+                           'Games': 'nunique'}).sort_values('Medal')
 
-b = dados.describe()
 print(b)
