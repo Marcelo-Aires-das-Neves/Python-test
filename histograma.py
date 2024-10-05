c46514fd58a0b388d6c2d0a1955f144fed1f79fb
marcelo aires das neves <mneves230@gmail.com>
2024-10-05 12:35:04 -0300

    test

--- histograma.py
+++ histograma.py
@@ -2,5 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dados = pd.read_csv('C:/Users/USER/Downloads/archive/athlete_events.csv')
-dados.hist(column='Age', bins=112)
+dados.hist(column='Height')
 plt.show()
