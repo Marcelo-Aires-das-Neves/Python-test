c3a27ac2aa2448a40e165596abdb947829167759
marcelo aires das neves <mneves230@gmail.com>
2024-10-08 19:51:58 -0300

    test

--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 
 dados = pd.read_csv('C:/Users/USER/Downloads/archive/athlete_events.csv')
-dados.boxplot(column='Age', by='Sex')
+dados.boxplot(column='Height')
 plt.show()
