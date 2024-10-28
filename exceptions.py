9a64d8e32cfc297872971f90cf0b2068f6f2e2f3
marcelo aires das neves <mneves230@gmail.com>
2024-10-28 16:44:11 -0300

    test

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,11 @@
-try:
-    x = int(input("Enter a number: "))
+while True:
+    
+    try:
+        x = int(input("Enter a number: "))
 
-except ValueError: 
-    print("Invalid input")
-else:
-    print(f"x = {x}")
+    except ValueError: 
+        print("Invalid input")
+    else:
+        break   
+    
+print(f"x = {x}")
