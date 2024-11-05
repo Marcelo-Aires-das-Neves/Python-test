3e37cf2191a963da9eac1675e44a86d691c1e792
marcelo aires das neves <mneves230@gmail.com>
2024-11-05 16:30:16 -0300

    test

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-x
+Voyager 5
--- test.py
+++ test.py
@@ -1,12 +1,28 @@
+distances = {
+    "Voyager 1": 163,
+    "Voyager 2": 1,
+    "Voyager 3": 2, 
+    "Voyager 4": 3,
+    "Voyager 5": "4 kk"
+}
+
 def main():
-    height = int(input("Enter the height of the triangle: "))
-    pyramid(height)
-            
-def pyramid(height):
-    for i in range(height + 1):
-        print("#" * i)
-              
+    spacecraft = input("Enter the spacecraft name: ")
+    try:
+        m = float(distances[spacecraft])
+    except KeyError:
+        print("Spacecraft not found")
+    except ValueError:
+        print("Invalid input")
+    else:
+        print(f"{spacecraft} is {convert_to_miles(m)} miles away")  
+    """    
+    finally:
+        print("Thanks for using this program")   """ 
+def convert_to_miles(km):   
+    return km * 149597870700
+
+
 
 if __name__ == "__main__":
-    main()
-
+    main()  
