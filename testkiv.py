b960cbcbf1dcbbc8bf77ef1817ffd6244bc1100b
marcelo aires das neves <mneves230@gmail.com>
2024-11-11 18:16:57 -0300

    test

--- testkiv.py
+++ testkiv.py
@@ -5,11 +5,21 @@ from kivy.core.window import Window
 Window.size = (600, 500)
 Window.clearcolor = (233/255, 225/255, 216/255, 1)
 
+usuarios = {}
+
 class Cadastro(BoxLayout):
     def cadastrar(self):
         nome = self.ids.nome.text
         saldo = self.ids.saldo.text
-    
+        if nome in usuarios:
+            self.ids.msg.text = "Usuário já cadastrado"
+        else:
+            usuarios[nome] = float(saldo)
+            self.ids.msg.text = "Usuário cadastrado com sucesso"
+            self.ids.nome.text = ""
+            self.ids.saldo.text = ""
+            print(usuarios)
+        
 class Testkiv(App):
     def build(self):
         return  Cadastro()
