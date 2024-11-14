c10fa0e9f507a808c4a3ae3c701a7fcdd7660f29
marcelo aires das neves <mneves230@gmail.com>
2024-11-14 08:54:53 -0300

    test

---
+++ sayings.py
@@ -0,0 +1,14 @@
+def main():
+    hello("Alice")
+    goodbye("Bob")
+
+def hello(name):
+    print("Hello, " + name + "!")
+      
+def goodbye(name):
+    print("Goodbye, " + name + "!")
+    
+    
+    
+if __name__ == "__main__":  
+    main()
