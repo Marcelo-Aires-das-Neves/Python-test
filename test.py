8de801f99bab181f1802fcb70f9bd199b5f51610
marcelo aires das neves <mneves230@gmail.com>
2024-11-27 09:56:17 -0300

    test

--- test.py
+++ test.py
@@ -1,16 +1,35 @@
 import random
 
-# Gera 8 números ímpares
-impares = random.sample([x for x in range(1, 26, 1)], 7)
+# Números já utilizados, separados por paridade
+pares_usados = [12, 18, 20, 24]
+impares_usados = [1, 3, 9, 11, 13]
 
-# Gera 7 números pares
-pares = random.sample([x for x in range(2, 26, 1)], 9)
+# Listas para armazenar os números finais
+todos_numeros = []
+pares_restantes = []
+impares_restantes = []
 
-# Combina as listas
-numeros = impares + pares
+# Função para gerar um número aleatório dentro de um intervalo, excluindo os já usados
+def gerar_numero(lista_usados, inicio, fim):
+    while True:
+        numero = random.randint(1, 25)
+        if numero not in lista_usados:
+            return numero
 
-# Embaralha a lista
-random.shuffle(numeros)
-numeros.sort()
-print(numeros)
+# Gerar os pares faltantes
+while len(pares_restantes) < 7:
+    novo_par = gerar_numero(pares_usados, 2, 24)
+    pares_restantes.append(novo_par)
+    pares_usados.append(novo_par)
 
+# Gerar os ímpares faltantes
+while len(impares_restantes) < 8:
+    novo_impar = gerar_numero(impares_usados, 1, 25)
+    impares_restantes.append(novo_impar)
+    impares_usados.append(novo_impar)
+
+# Combinar as listas de pares e ímpares
+todos_numeros.extend(pares_restantes)
+todos_numeros.extend(impares_restantes)
+
+print(todos_numeros)
