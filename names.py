cd275f699dcaf938a18d0c4a3abefe88ece937fb
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2024-12-10 00:14:41 +0000

    test

--- names.py
+++ names.py
@@ -1,5 +1,5 @@
 names = ""
 for _ in range(3):
-     names = (input("What's your name? "))
+    name = input("What's your name? ")
     with open("names.txt", "a") as file:
-        file.write(f"{names} \n")
+        file.write(f"{name}\n")
